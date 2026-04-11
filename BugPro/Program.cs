47e49b135f9f4eef741b114ff4334a02703fa779
MrStef989 <borodovgerman123@gmail.com>
2026-04-11 21:05:18 +0300

    new

--- BugPro/Program.cs
+++ BugPro/Program.cs
@@ -4,12 +4,23 @@ namespace BugPro
 {
     public enum State
     {
-        Open, Assigned, InProgress, Resolved, Closed, Reopened
+        New,
+        Triage,
+        Fixing,
+        Closed,
+        Reopened
     }
 
     public enum Trigger
     {
-        Assign, Start, Resolve, Close, Reopen
+        Open,
+        Fix,
+        Defer,
+        CloseInvalid,
+        Solved,
+        NotSolved,
+        Reopen,
+        Return
     }
 
     public class Bug
@@ -17,57 +28,73 @@ namespace BugPro
         private StateMachine<State, Trigger> _machine;
         public State CurrentState => _machine.State;
 
-        public Bug(State initialState = State.Open)
+        public Bug(State initialState = State.New)
         {
             _machine = new StateMachine<State, Trigger>(initialState);
 
-            _machine.Configure(State.Open)
-                .Permit(Trigger.Assign, State.Assigned);
+            _machine.Configure(State.New)
+                .Permit(Trigger.Open, State.Triage);
 
-            _machine.Configure(State.Assigned)
-                .Permit(Trigger.Start, State.InProgress)
-                .Permit(Trigger.Reopen, State.Reopened);
+            _machine.Configure(State.Triage)
+                .Permit(Trigger.Fix, State.Fixing)
+                .PermitReentry(Trigger.Defer)
+                .Permit(Trigger.CloseInvalid, State.Closed);
 
-            _machine.Configure(State.InProgress)
-                .Permit(Trigger.Resolve, State.Resolved)
-                .Permit(Trigger.Assign, State.Assigned);
-
-            _machine.Configure(State.Resolved)
-                .Permit(Trigger.Close, State.Closed)
-                .Permit(Trigger.Reopen, State.Reopened);
+            _machine.Configure(State.Fixing)
+                .Permit(Trigger.Solved, State.Closed)
+                .Permit(Trigger.NotSolved, State.Reopened);
 
             _machine.Configure(State.Closed)
-                .Permit(Trigger.Reopen, State.Reopened);
+                .Permit(Trigger.Return, State.Triage);
 
             _machine.Configure(State.Reopened)
-                .Permit(Trigger.Assign, State.Assigned);
+                .Permit(Trigger.Reopen, State.Triage);
         }
 
-        public void Assign()   => _machine.Fire(Trigger.Assign);
-        public void Start()    => _machine.Fire(Trigger.Start);
-        public void Resolve()  => _machine.Fire(Trigger.Resolve);
-        public void Close()    => _machine.Fire(Trigger.Close);
-        public void Reopen()   => _machine.Fire(Trigger.Reopen);
+        public void Open()         => _machine.Fire(Trigger.Open);
+        public void Fix()          => _machine.Fire(Trigger.Fix);
+        public void Defer()        => _machine.Fire(Trigger.Defer);
+        public void CloseInvalid() => _machine.Fire(Trigger.CloseInvalid);
+        public void Solved()       => _machine.Fire(Trigger.Solved);
+        public void NotSolved()    => _machine.Fire(Trigger.NotSolved);
+        public void Reopen()       => _machine.Fire(Trigger.Reopen);
+        public void Return()       => _machine.Fire(Trigger.Return);
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            var bug = new Bug();
-            Console.WriteLine($"State: {bug.CurrentState}");
-            bug.Assign();
-            Console.WriteLine($"State: {bug.CurrentState}");
-            bug.Start();
-            Console.WriteLine($"State: {bug.CurrentState}");
-            bug.Resolve();
-            Console.WriteLine($"State: {bug.CurrentState}");
-            bug.Close();
-            Console.WriteLine($"State: {bug.CurrentState}");
-            bug.Reopen();
-            Console.WriteLine($"State: {bug.CurrentState}");
-            bug.Assign();
-            Console.WriteLine($"State: {bug.CurrentState}");
+            Console.WriteLine("=== Bug Workflow Demo ===\n");
+
+            Console.WriteLine("Scenario 1: Bug fixed successfully");
+            var bug1 = new Bug();
+            Console.WriteLine($"  {bug1.CurrentState}");
+            bug1.Open();
+            Console.WriteLine($"  Open -> {bug1.CurrentState}");
+            bug1.Fix();
+            Console.WriteLine($"  Fix -> {bug1.CurrentState}");
+            bug1.Solved();
+            Console.WriteLine($"  Solved -> {bug1.CurrentState}");
+
+            Console.WriteLine("\nScenario 2: Bug closed as invalid");
+            var bug2 = new Bug();
+            bug2.Open();
+            Console.WriteLine($"  Open -> {bug2.CurrentState}");
+            bug2.CloseInvalid();
+            Console.WriteLine($"  CloseInvalid -> {bug2.CurrentState}");
+            bug2.Return();
+            Console.WriteLine($"  Return -> {bug2.CurrentState}");
+
+            Console.WriteLine("\nScenario 3: Bug not solved, reopened");
+            var bug3 = new Bug();
+            bug3.Open();
+            bug3.Fix();
+            Console.WriteLine($"  Fixing -> {bug3.CurrentState}");
+            bug3.NotSolved();
+            Console.WriteLine($"  NotSolved -> {bug3.CurrentState}");
+            bug3.Reopen();
+            Console.WriteLine($"  Reopen -> {bug3.CurrentState}");
         }
     }
 }
--- BugTests/UnitTest1.cs
+++ BugTests/UnitTest1.cs
@@ -7,122 +7,113 @@ namespace BugTests
     public class UnitTest1
     {
         [TestMethod]
-        public void Test01_InitialStateIsOpen()
+        public void Test01_InitialStateIsNew()
         {
             var bug = new Bug();
-            Assert.AreEqual(State.Open, bug.CurrentState);
+            Assert.AreEqual(State.New, bug.CurrentState);
         }
 
         [TestMethod]
-        public void Test02_AssignFromOpen()
+        public void Test02_OpenMovesToTriage()
         {
             var bug = new Bug();
-            bug.Assign();
-            Assert.AreEqual(State.Assigned, bug.CurrentState);
+            bug.Open();
+            Assert.AreEqual(State.Triage, bug.CurrentState);
         }
 
         [TestMethod]
-        public void Test03_StartFromAssigned()
+        public void Test03_FixMovesToFixing()
         {
             var bug = new Bug();
-            bug.Assign();
-            bug.Start();
-            Assert.AreEqual(State.InProgress, bug.CurrentState);
+            bug.Open();
+            bug.Fix();
+            Assert.AreEqual(State.Fixing, bug.CurrentState);
         }
 
         [TestMethod]
-        public void Test04_ResolveFromInProgress()
+        public void Test04_SolvedMovesToClosed()
         {
             var bug = new Bug();
-            bug.Assign();
-            bug.Start();
-            bug.Resolve();
-            Assert.AreEqual(State.Resolved, bug.CurrentState);
-        }
-
-        [TestMethod]
-        public void Test05_CloseFromResolved()
-        {
-            var bug = new Bug();
-            bug.Assign();
-            bug.Start();
-            bug.Resolve();
-            bug.Close();
+            bug.Open();
+            bug.Fix();
+            bug.Solved();
             Assert.AreEqual(State.Closed, bug.CurrentState);
         }
 
         [TestMethod]
-        public void Test06_ReopenFromClosed()
+        public void Test05_CloseInvalidMovesToClosed()
         {
             var bug = new Bug();
-            bug.Assign();
-            bug.Start();
-            bug.Resolve();
-            bug.Close();
-            bug.Reopen();
+            bug.Open();
+            bug.CloseInvalid();
+            Assert.AreEqual(State.Closed, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void Test06_NotSolvedMovesToReopened()
+        {
+            var bug = new Bug();
+            bug.Open();
+            bug.Fix();
+            bug.NotSolved();
             Assert.AreEqual(State.Reopened, bug.CurrentState);
         }
 
         [TestMethod]
-        public void Test07_ReopenFromResolved()
+        public void Test07_ReopenMovesToTriage()
         {
             var bug = new Bug();
-            bug.Assign();
-            bug.Start();
-            bug.Resolve();
+            bug.Open();
+            bug.Fix();
+            bug.NotSolved();
             bug.Reopen();
-            Assert.AreEqual(State.Reopened, bug.CurrentState);
+            Assert.AreEqual(State.Triage, bug.CurrentState);
         }
 
         [TestMethod]
-        public void Test08_AssignFromReopened()
+        public void Test08_ReturnFromClosedToTriage()
         {
             var bug = new Bug();
-            bug.Assign();
-            bug.Start();
-            bug.Resolve();
-            bug.Close();
-            bug.Reopen();
-            bug.Assign();
-            Assert.AreEqual(State.Assigned, bug.CurrentState);
+            bug.Open();
+            bug.CloseInvalid();
+            bug.Return();
+            Assert.AreEqual(State.Triage, bug.CurrentState);
         }
 
         [TestMethod]
-        public void Test09_ReassignFromInProgress()
+        public void Test09_DeferStaysInTriage()
         {
             var bug = new Bug();
-            bug.Assign();
-            bug.Start();
-            bug.Assign();
-            Assert.AreEqual(State.Assigned, bug.CurrentState);
+            bug.Open();
+            bug.Defer();
+            Assert.AreEqual(State.Triage, bug.CurrentState);
         }
 
         [TestMethod]
-        public void Test10_FullCycleWithReopen()
+        public void Test10_FullCycleReopenAndFix()
         {
             var bug = new Bug();
-            bug.Assign();
-            bug.Start();
-            bug.Resolve();
-            bug.Close();
+            bug.Open();
+            bug.Fix();
+            bug.NotSolved();
             bug.Reopen();
-            bug.Assign();
-            bug.Start();
-            Assert.AreEqual(State.InProgress, bug.CurrentState);
+            bug.Fix();
+            bug.Solved();
+            Assert.AreEqual(State.Closed, bug.CurrentState);
         }
 
         [TestMethod]
         public void Test11_InvalidTransitionThrows()
         {
             var bug = new Bug();
-            Assert.ThrowsException<InvalidOperationException>(() => bug.Start());
+            Assert.ThrowsException<InvalidOperationException>(() => bug.Fix());
         }
 
         [TestMethod]
-        public void Test12_InitialStateAssigned()
+        public void Test12_InitialStateSetToTriage()
         {
-            var bug = new Bug(State.Assigned);
-            Assert.AreEqual(State.Assigned, bug.CurrentState);
+            var bug = new Bug(State.Triage);
+            Assert.AreEqual(State.Triage, bug.CurrentState);
         }
     }
 }
